2fb070f2c092d20c37b5e697de380581261eb447
Akashgoyal55 <agoyal1@sfsu.edu>
2024-06-25 18:24:10 -0700

    adding files

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById('start-button').addEventListener('click', function() {
+    document.getElementById('welcome-page').classList.add('hidden');
+    document.getElementById('todo-page').classList.remove('hidden');
+    document.getElementById('todo-page').classList.add('active');
+});
+
+document.getElementById('add-button').addEventListener('click', function() {
+    const todoInput = document.getElementById('todo-input');
+    const todoText = todoInput.value.trim();
+
+    if (todoText !== '') {
+        const todoList = document.getElementById('todo-list');
+        const listItem = document.createElement('li');
+
+        const textNode = document.createTextNode(todoText);
+        listItem.appendChild(textNode);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.classList.add('delete-button');
+        deleteButton.addEventListener('click', function() {
+            todoList.removeChild(listItem);
+        });
+
+        listItem.appendChild(deleteButton);
+        listItem.addEventListener('click', function() {
+            listItem.classList.toggle('completed');
+        });
+
+        todoList.appendChild(listItem);
+        todoInput.value = '';
+    }
+});
